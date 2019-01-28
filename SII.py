bac0ba35877cdaad6414a8cde5af6639eb8b1566
Mauricio Acencio <m.acencio01@ufromail.cl>
2019-01-28 17:09:24 -0300

    Extracción nombres de carpetas y archivos

--- SII.py
+++ SII.py
@@ -10,10 +10,13 @@ import os
 # Este script se ejecuta dentro de la carpeta que, supuestamente, contiene únicamente las carpetas con el
 # nombre y archivos correspondientes a cada cliente
 
-# Listado de caarpetas de clientes
+# Listado de carpetas de clientes
 
 carpetas = next(os.walk("."))[1]
 
-# Lectura de archivos por carpeta
-print next(os.walk("."))[2]
-# disk.csv
+# Nombres de archivos por carpeta
+# (Extrae los nombres de los archivos pedidos desde una de las carpetas, esto suponiendo que los nombres
+# son todos consistentes y los mismos en cada carpeta)
+
+archivos = next(os.walk(carpetas[0]))[2]
+
