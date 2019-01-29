10fce844e2f6e93c4142c9073ba017c2da6ecbfd
Mauricio Acencio <m.acencio01@ufromail.cl>
2019-01-29 11:36:20 -0300

    Arreglado problema de primera línea en blanco

--- test.py
+++ test.py
@@ -3,9 +3,18 @@
 
 import pandas, csv
 
-df = pandas.read_csv('Ivette  Pascual Robin/red (copy).csv')
-print df
-with open('Ivette  Pascual Robin/red (copy).csv', mode='r') as archivo:
-	csvreader = csv.reader(archivo)
-	for row in csvreader:
-		print row
+revise = None
+
+with open('Ivette  Pascual Robin/red.csv', mode='r') as archivo:
+	revise1 = [line.replace('\0','').replace('\r','').replace('\n','') for line in archivo]
+	revise = [line for line in revise1[1:] if line.strip()]
+print revise1
+print revise
+for l in revise:
+	print "-> %s" % l
+print "Done"
+
+# ~ with open('test2.csv', 'w') as myfile:
+    # ~ wr = csv.writer(myfile)
+    # ~ for r in revise:
+		# ~ wr.writerow(r.split(","))
